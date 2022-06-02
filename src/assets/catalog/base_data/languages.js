0dc2eb0a4d25c6b844680d3e5c71d3a9ac9b1bb4
dimalagoda <dimalagoda@gmail.com>
2022-06-03 02:08:04 +0300

    langs_all (almost)

--- src/assets/catalog/base_data/languages.js
+++ src/assets/catalog/base_data/languages.js
@@ -1,4 +1,5 @@
 export const lang = {
+
     common: {
         name: "lang_common",
         details: "lang_common_details"
@@ -31,5 +32,45 @@ export const lang = {
         name: "lang_dragon",
         details: "lang_dragon_details"
     },
-    
+    giant: {
+        name: "lang_giant",
+        details: "lang_giant_details"
+    },
+    goblin: {
+        name: "lang_goblin",
+        details: "lang_goblin_details"
+    },
+    undercommon: {
+        name: "lang_undercommon",
+        details: "lang_undercommon_details"
+    },
+    sylvan: {
+        name: "lang_sylvan",
+        details: "lang_sylvan_details"
+    },
+    animal: {
+        name: "lang_animal",
+        details: "lang_animal_details"
+    },
+    druid: {
+        name: "lang_druid",
+        details: "lang_druid_details"
+    },
+    primordial: {
+        name: "lang_primordial",
+        details: "lang_primordial_details"
+    },
+    sky: {
+        name: "lang_sky",
+        details: "lang_sky_details"
+    },
+    demonic: {
+        name: "lang_demonic",
+        details: "lang_demonic_details"
+    },
+    thieves: {
+        name: "lang_thieves",
+        details: "lang_thieves_details"
+    },
+
 }
