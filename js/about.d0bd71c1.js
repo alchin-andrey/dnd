25be15f4e203934899a95c5b6ff04210726e392e
alchin_andrey <immortiale@gmail.com>
2022-06-08 01:20:24 +0300

    v_0.4

---
+++ js/about.d0bd71c1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdnd"]=self["webpackChunkdnd"]||[]).push([[443],{97381:function(n,u,t){t.r(u),t.d(u,{default:function(){return f}});var a=t(66252);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var d=t(83744);const i={},o=(0,d.Z)(i,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=about.d0bd71c1.js.map
