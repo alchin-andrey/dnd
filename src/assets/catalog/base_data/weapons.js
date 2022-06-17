00e5fd5270bce320cec619f702f7c4cb25c659c7
dimalagoda <dimalagoda@gmail.com>
2022-06-17 20:55:07 +0300

    weapons

--- src/assets/catalog/base_data/weapons.js
+++ src/assets/catalog/base_data/weapons.js
@@ -1,23 +1,23 @@
 export default {
 
     battle_axes:{
-        name:"battle_axes",
-        details:"battle_axes_details",
+        name:"weapons_battle_axes",
+        details:"weapons_battle_axes_details",
     },
 
     hand_axes:{
-        name:"hand_axes",
-        details:"hand_axes_details",
+        name:"weapons_hand_axes",
+        details:"weapons_hand_axes_details",
     },
 
     battle_hammers:{
-        name:"battle_hammers",
-        details:"battle_hammers_details",
+        name:"weapons_battle_hammers",
+        details:"weapons_battle_hammers_details",
     },
 
     light_hammers:{
-        name:"light_hammers",
-        details:"light_hammers_details",
+        name:"weapons_light_hammers",
+        details:"weapons_light_hammers_details",
     },
 
 }
