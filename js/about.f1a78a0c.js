1480b289d1a578ef01bccb537e9de69e95000fbe
Alchin Andrey <immortiale@gmail.com>
2022-08-11 22:33:07 +0300

    dist

---
+++ js/about.f1a78a0c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdnd"]=self["webpackChunkdnd"]||[]).push([[443],{697381:function(n,u,t){t.r(u),t.d(u,{default:function(){return f}});var a=t(166252);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var d=t(983744);const i={},o=(0,d.Z)(i,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=about.f1a78a0c.js.map
