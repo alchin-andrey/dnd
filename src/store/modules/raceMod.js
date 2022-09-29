96292a8b1f9dbae85591bc97859177af194b7da4
Alchin Andrey <immortiale@gmail.com>
2022-09-29 16:08:55 +0300

    receMod

--- src/store/modules/raceMod.js
+++ src/store/modules/raceMod.js
@@ -1,11 +1,11 @@
 import race from "@/assets/catalog/base_data/races.js";
 
 export default {
-  namespaced: true,
-  state: () => ({
-    race: race,
-  }),
-  getters: {
+	namespaced: true,
+	state: () => ({
+		race: race,
+	}),
+	getters: {
 
-  },
+	},
 };
