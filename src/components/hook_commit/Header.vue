30918a1e3c37447946b5a7bced938a08d80ec5f8
Alchin Andrey <immortiale@gmail.com>
2022-11-23 03:29:46 +0300

    Header_Setup_Done

--- src/components/hook_commit/Header.vue
+++ src/components/hook_commit/Header.vue
@@ -1,85 +1,63 @@
 <template>
-	<div class="header">
-		<my-logo-card @click="showSettings__Main('logo')" :active="main_page.shown.logo" />
-		<div class="header_col">
-			<my-header-card
-				@click="showSettings__Main('lang')"
-				:active="main_page.shown.lang"
-			>
-				<!-- <emoji v-for="n in em_Icon" :key="n"
+  <div class="header">
+    <my-logo-card @click="showSettings__Main('logo')" :active="main_page.shown.logo" />
+    <div class="header_col">
+      <my-header-card @click="showSettings__Main('lang')" :active="main_page.shown.lang">
+        <!-- <emoji v-for="n in Em_Icon" :key="n"
 					:data="emojiIndex"
 					:emoji="n"
 					:set="set_emoji"
 					:size="15"
 				/> -->
-				<img class="header_icon" :src="Lang_Icon" alt="Lang_Icon" />
-			</my-header-card>
-			<my-header-card
-				@click="showSettings__Main('lvl')"
-				:active="main_page.shown.lvl"
-				:slots="Char_Lvl"
-			/>
-		</div>
-	</div>
+        <img class="header_icon" :src="Lang_Icon" alt="Lang_Icon" />
+      </my-header-card>
+      <my-header-card
+        @click="showSettings__Main('lvl')"
+        :active="main_page.shown.lvl"
+        :slots="Char_Lvl"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { useStore } from "vuex";
+import { computed, reactive } from "vue";
 import { useShowSettings } from "@/hooks/PAGES/common/useShowSettings.js";
-import { mapStateLib, mapGettersLib } from "@/hooks/stateLibName";
+
+const store = useStore();
+const main_page = computed(() => store.state.pages.main_page);
+
+const Em_Icon = computed(() => store.getters["dic/Em_Icon"]);
+const Lang_Icon = computed(() => store.getters["dic/Lang_Icon"]);
+
+const MY = reactive(store.state.MY.MY);
+const Char_Lvl = computed(() => `lvl ${MY.level}`);
+
 const { showSettings__Main } = useShowSettings();
-
-const { race_page } = mapStateLib('pages')
-const { shown_Home } = mapGettersLib('pages')
-// console.log("map.race_page", race_page.value)
-// console.log("map.shown_Home", shown_Home.value)
-// const showSettings_Main = (name) => showSettings("main_page", name)
-// function showSettings_Main (name) {
-// 	showSettings("main_page", name)
-// }
-</script>
-
-<script>
-import { mapState, mapGetters } from "vuex";
-export default {
-	// name: "Header",
-	computed: {
-		...mapState({
-			MY: (state) => state.MY.MY,
-			main_page: (state) => state.pages.main_page,
-			dic: (state) => state.dic.dic,
-		}),
-
-		...mapGetters("dic", ["Em_Icon", "Lang_Icon"]),
-
-		Char_Lvl() {
-			// console.log("map.shown_Home", this.shown_Home)
-			return `lvl ${this.MY.level}`;
-		},
-	},
-};
 </script>
 
 <style scoped>
 .header {
-	height: 28px;
-	margin: 0 0 22px 0;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
+  height: 28px;
+  margin: 0 0 22px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
 .header_col {
-	height: 100%;
-	display: flex;
-	gap: 4px;
+  height: 100%;
+  display: flex;
+  gap: 4px;
 }
 
 .header_icon {
-	height: 18px;
+  height: 18px;
 }
 
 .emoji-mart-emoji {
-	padding: 0;
-	line-height: 0;
+  padding: 0;
+  line-height: 0;
 }
 </style>
--- src/hooks/stateLibName.js
+++ src/hooks/stateLibName.js
@@ -1,35 +1,57 @@
-import { computed, reactive } from 'vue'
-import { useStore } from 'vuex'
+import { computed, reactive } from "vue";
+import { useStore } from "vuex";
 
-const mapStateLib = (module) => {
-  const store = useStore()
-  return Object.fromEntries(Object.keys(store.state[module]).map(key => (module !== undefined) ? [key, computed(() => store.state[module][key])] : [key, computed(() => store.state[key])]))
-}
+const mapStateLib = module => {
+	const store = useStore();
+	return Object.fromEntries(
+		Object.keys(store.state[module]).map(key =>
+			module !== undefined
+				? [key, computed(() => store.state[module][key])]
+				: [key, computed(() => store.state[key])]
+		)
+	);
+};
 
-// const mapStateLib = (module) => {
-//   const store = useStore()
-//   return Object.fromEntries(Object.keys(store.state[module]).map(key => (module !== undefined) ? [key, reactive(store.state[module][key])] : [key, reactive(store.state[key])]))
-// }
+const mapGettersLib = module => {
+	const store = useStore();
+	return Object.fromEntries(
+		Object.keys(store.getters).map(getter =>
+			module !== undefined && getter.startsWith(module)
+				? [
+						getter.substr(module.length + 1),
+						computed(() => store.getters[getter]),
+				  ]
+				: [getter, computed(() => store.getters[getter])]
+		)
+	);
+};
 
-// const mapGettersLib = (module) => {
-//   const store = useStore()
-//   return Object.fromEntries(Object.keys(store.getters).map(getter => (module !== undefined && getters.startsWith(module)) ? [getters.substr(module.length+1), computed(() => store.getters[getter])] : [getter, computed(() => store.getters[getter])]))
-// }
+const mapMutationsLib = module => {
+	const store = useStore();
+	return Object.fromEntries(
+		Object.keys(store._mutations[module]).map(mutation =>
+			module !== undefined && mutation.startsWith(module)
+				? [
+						mutation.substr(module.length + 1),
+						value => store.commit(mutation, value),
+				  ]
+				: [mutation, value => store.commit(mutation, value)]
+		)
+	);
+};
 
-const mapGettersLib = (module) => {
-  const store = useStore()
-  return Object.fromEntries(Object.keys(store.getters).map(getter => (module !== undefined && getter.startsWith(module)) ? [getter.substr(module.length+1), computed(() => store.getters[getter])] : [getter, computed(() => store.getters[getter])]))
-}
+const mapActionsLib = module => {
+	const store = useStore();
+	return Object.fromEntries(
+		Object.keys(store._actions).map(action =>
+			module !== undefined && action.startsWith(module)
+				? [
+						action.substr(module.length + 1),
+						value => store.dispatch(action, value),
+				  ]
+				: [action, value => store.dispatch(action, value)]
+		)
+	);
+};
 
-
-const mapMutationsLib = (module) => {
-  const store = useStore()
-  return Object.fromEntries(Object.keys(store._mutations[module]).map(mutation => (module !== undefined && mutation.startsWith(module)) ? [mutation.substr(module.length+1), value => store.commit(mutation, value)] : [mutation, value => store.commit(mutation, value)]))
-}
-
-const mapActionsLib = (module) => {
-  const store = useStore()
-  return Object.fromEntries(Object.keys(store._actions).map(action => (module !== undefined && action.startsWith(module)) ? [action.substr(module.length+1), value => store.dispatch(action, value)] : [action, value => store.dispatch(action, value)] ))
-}
-
-export { mapStateLib, mapGettersLib, mapMutationsLib, mapActionsLib }
+export { mapStateLib, mapGettersLib, mapMutationsLib, mapActionsLib };
