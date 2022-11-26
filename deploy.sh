9b6f2df9b9567c22c764996d905a21bae5be49b1
Alchin Andrey <immortiale@gmail.com>
2022-11-26 14:36:20 +0300

    dep

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:https://github.com/alchin-andrey/dnd.git store:gh-pages
 
 cd -
