baf0c7afefa59b9a050d9a17d9c5d68d8d78e6c5
Alchin Andrey <immortiale@gmail.com>
2022-11-26 15:04:19 +0300

    pass-2

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/dnd/',
-  // publicPath: process.env.NODE_ENV === 'production' ? '/dnd/' : '/',
-  // publicPath: process.env.NODE_ENV === 'production' ? '/dnd/' : '/',
+  // base: '/dnd/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/dnd/' : '/',
+  // publicPath: '/dnd/',
   transpileDependencies: true
 })
 
