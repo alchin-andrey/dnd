e1eee339db1a2f10c6e4f9faf542ff5cb323a819
Alchin Andrey <immortiale@gmail.com>
2022-11-26 14:25:39 +0300

    pyb

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/dnd/' : '/',
   transpileDependencies: true
 })
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/'
+// }
