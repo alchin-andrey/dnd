b2b280698693d38343089d08a4923538c4065b74
Alchin Andrey <immortiale@gmail.com>
2022-12-06 21:24:26 +0300

    svg

--- src/assets/catalog/icon/atribute_icon.js
+++ src/assets/catalog/icon/atribute_icon.js
@@ -18,4 +18,11 @@ export default {
 	vision_night: '<circle cx="9" cy="9" r="2.5" stroke="white"/><path d="M14 9L11 7V11L14 9Z" fill="white"/><path d="M4 9L7 11L7 7L4 9Z" fill="white"/>',
 
 	hp_bonus: '<path fill-rule="evenodd" clip-rule="evenodd" d="M10 5H8V8L5 8V10H8V13H10V10L13 10V8L10 8V5Z" fill="white"/>',
+
+  regen_bonus: '<path fill-rule="evenodd" clip-rule="evenodd" d="M9 3C5.68629 3 3 5.68629 3 9C3 12.3137 5.68629 15 9 15C12.3137 15 15 12.3137 15 9C15 5.68629 12.3137 3 9 3ZM8 8V5H10V8L13 8V10L10 10V13H8V10H5V8H8Z" fill="white"/>',
+
+  armor_class: '<path d="M9 5H13V11L9 14L5 11V5H9Z" fill="white"/>',
+
+  initiative: '<path fill-rule="evenodd" clip-rule="evenodd" d="M10 5H8V11L5 11V13L8 13L9 13L10 13H13V11H10V5Z" fill="white"/>',
+
 };
