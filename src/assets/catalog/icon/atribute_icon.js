070a36dc8bd444b9389019d60e5d016082cd77ca
Alchin Andrey <immortiale@gmail.com>
2022-12-16 12:48:18 +0300

    intelligence <rect>

--- src/assets/catalog/icon/atribute_icon.js
+++ src/assets/catalog/icon/atribute_icon.js
@@ -8,7 +8,7 @@ export default {
   
   charisma: '<path d="M9 4L10.1803 7.81966H14L10.9098 10.1803L12.0902 14L9 11.6393L5.90983 14L7.09017 10.1803L4 7.81966H7.81966L9 4Z"/>',
 
-	intelligence: '<path d="M5.5,5.5h7v7h-7Z" rx="3.5" />',
+	intelligence: '<rect x="5.5" y="5.5" width="7" height="7" rx="3.5" />',
   
 	wisdom: '<path d="M4.71,9l4.29,-4.29l4.29,4.29l-4.29,4.29Z" />',
   //STATS
