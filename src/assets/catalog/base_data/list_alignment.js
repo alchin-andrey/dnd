22442050d452bb2dba3030bcb79134ef41392430
dimalagoda <dimalagoda@gmail.com>
2023-01-26 02:36:42 +0200

    step3_alignment

--- src/assets/catalog/base_data/list_alignment.js
+++ src/assets/catalog/base_data/list_alignment.js
@@ -1,187 +1,197 @@
 export default {
+  alignment: [
+    { name: "principal_altruistic", details: "principal_altruistic_details" },
+    { name: "principal_neutral", details: "principal_neutral_details" },
+    { name: "principal_egoistic", details: "principal_neutral_egoistic" },
 
-    alignment: [
-        { name: "principal_altruistic", details: "principal_altruistic_details",},
-        { name: "principal_neutral", details: "principal_neutral_details",},
-        { name: "principal_egoistic", details: "principal_neutral_egoistic",},
+    { name: "fluid_altruistic", details: "fluid_altruistic_details" },
+    { name: "fluid_neutral", details: "fluid_neutral_details" },
+    { name: "fluid_egoistic", details: "fluid_neutral_egoistic" },
 
-        { name: "fluid_altruistic", details: "fluid_altruistic_details",},
-        { name: "fluid_neutral", details: "fluid_neutral_details",},
-        { name: "fluid_egoistic", details: "fluid_neutral_egoistic",},
+    {
+      name: "unprincipal_altruistic",
+      details: "unprincipal_altruistic_details",
+    },
+    { name: "unprincipal_neutral", details: "unprincipal_neutral_details" },
+    { name: "unprincipal_egoistic", details: "unprincipal_neutral_egoistic" },
+  ],
 
-        { name: "unprincipal_altruistic", details: "unprincipal_altruistic_details",},
-        { name: "unprincipal_neutral", details: "unprincipal_neutral_details",},
-        { name: "unprincipal_egoistic", details: "unprincipal_neutral_egoistic",},
-    ],
+  main_feature: [
+    { name: "feature_taler" }, // рассказчик
+    { name: "feature_gossip" }, // сплетник
+    { name: "feature_flatterer" }, // льстец
+    { name: "feature_witty" }, // остроумний
+    { name: "feature_boastful" }, // хвастливьій
+    { name: "feature_sarcastic" }, // саркастичний
+    { name: "feature_vulgar" }, // пошлий
+    { name: "feature_obscene" }, // похабний
+    { name: "feature_toxic" }, // токсичний
+    { name: "feature_coldblooded" }, // хладнокровний
+    { name: "feature_schemer" }, // махинатор
+    { name: "feature_conspirator" }, // конспиролог
+    { name: "feature_fatalist" }, // фаталист
+    { name: "feature_philosopher" }, // философ
+    { name: "feature_erudite" }, // єрудированний
+    { name: "feature_wise" }, // мудрий
+    { name: "feature_industrious" }, // трудолюбивий
+    { name: "feature_thoughtful" }, // задумчивий
+    { name: "feature_silent" }, // молчаливий
+    { name: "feature_closed" }, // замкнутьій
+    { name: "feature_unsociable" }, // нелюдимий
+    { name: "feature_injured" }, // травмированний
+    { name: "feature_anxious" }, // тревожний
+    { name: "feature_incredulous" }, // недоверчивий
+    { name: "feature_vindictive" }, // злопамятний
+    { name: "feature_vengeful" }, // мстительний
+    { name: "feature_irascible" }, // вспильчивий
+    { name: "feature_harmful" }, // вредний
+    { name: "feature_cunning" }, // хитрий
+    { name: "feature_approach" }, // підступний
+    { name: "feature_cruel" }, // жорстокий
+    { name: "feature_pitiless" }, // безжальний
+    { name: "feature_rectilinear" }, // прямолинейний
+    { name: "feature_rude" }, // грубий
+    { name: "feature_high" }, // високомерний
+    { name: "feature_haughty" }, // надменний
+    { name: "feature_snob" }, // сноб
+    { name: "feature_perfectionist" }, // перфекционист
+    { name: "feature_neat" }, // чистюля
+    { name: "feature_polite" }, // вежливий
+    { name: "feature_peaceful" }, // миролюбний
+    { name: "feature_generous" }, // щедрий
+    { name: "feature_compassionate" }, // сердобольний
+    { name: "feature_inquisitive" }, // любопитний
+    { name: "feature_curious" }, // любознательний
+    { name: "feature_serene" }, // безмятежний
+    { name: "feature_clumsy" }, // неуклюжий
+    { name: "feature_careless" }, // небрежний
+    { name: "feature_fickle" }, // переменчивий
+    { name: "feature_active" }, // активний
+    { name: "feature_stubborn" }, // упорний
+    { name: "feature_braver" }, // завзятий
+    { name: "feature_competitive" }, // соревновательний
+    { name: "feature_brave" }, // храбрий
+    { name: "feature_adventurer" }, // авантюрист
+    { name: "feature_rebel" }, // бунтарь
+  ],
 
-    main_feature: [
-        
-        { name: "feature_taler",},// рассказчик
-        { name: "feature_gossip",},// сплетник
-        { name: "feature_flatterer",},// льстец
-        { name: "feature_witty",},// остроумний
-        { name: "feature_boastful",},// хвастливьій
-        { name: "feature_sarcastic",},// саркастичний
-        { name: "feature_vulgar",},// пошлий
-        { name: "feature_obscene",},// похабний
-        { name: "feature_toxic",},// токсичний
-        { name: "feature_coldblooded",},// хладнокровний 
-        { name: "feature_schemer",},// махинатор 
-        { name: "feature_conspirator",},// конспиролог 
-        { name: "feature_fatalist",},// фаталист 
-        { name: "feature_philosopher",},// философ 
-        { name: "feature_erudite",},// єрудированний   
-        { name: "feature_wise",},// мудрий
-        { name: "feature_industrious",},// трудолюбивий   
-        { name: "feature_thoughtful",},// задумчивий 
-        { name: "feature_silent",},// молчаливий
-        { name: "feature_closed",},// замкнутьій
-        { name: "feature_unsociable",},// нелюдимий
-        { name: "feature_injured",},// травмированний
-        { name: "feature_anxious",},// тревожний
-        { name: "feature_incredulous",},// недоверчивий 
-        { name: "feature_vindictive",},// злопамятний
-        { name: "feature_vengeful",},// мстительний
-        { name: "feature_irascible",},// вспильчивий 
-        { name: "feature_harmful",},// вредний 
-        { name: "feature_cunning",},// хитрий
-        { name: "feature_approach",},// підступний
-        { name: "feature_cruel",},// жорстокий
-        { name: "feature_pitiless",},// безжальний
-        { name: "feature_rectilinear",},// прямолинейний
-        { name: "feature_rude",},// грубий
-        { name: "feature_high",},// високомерний
-        { name: "feature_haughty",},// надменний
-        { name: "feature_snob",},// сноб
-        { name: "feature_perfectionist",},// перфекционист
-        { name: "feature_neat",},// чистюля
-        { name: "feature_polite",},// вежливий
-        { name: "feature_peaceful",},// миролюбний
-        { name: "feature_generous",},// щедрий
-        { name: "feature_compassionate",},// сердобольний 
-        { name: "feature_inquisitive",},// любопитний
-        { name: "feature_curious",},// любознательний
-        { name: "feature_serene",},// безмятежний 
-        { name: "feature_clumsy",},// неуклюжий
-        { name: "feature_careless",},// небрежний
-        { name: "feature_fickle",},// переменчивий
-        { name: "feature_active",},// активний
-        { name: "feature_stubborn",},// упорний
-        { name: "feature_acceptance",},// завзятий
-        { name: "feature_competitive",},// соревновательний
-        { name: "feature_brave",},// храбрий
-        { name: "feature_adventurer",},// авантюрист
-        { name: "feature_rebel",},// бунтарь 
-        
-        
+  ideals: [
+    // ІДЕАЛИ
 
-        // ІДЕАЛИ
+    { name: "ideal_justice" }, // справедливість
+    { name: "ideal_equality" }, // рівність
+    { name: "ideal_independence" }, // незалежність
+    { name: "ideal_liberty" }, // свобода
+    { name: "ideal_freedom" }, // воля
+    { name: "ideal_respect" }, // повага
+    { name: "ideal_society" }, // суспільство
+    { name: "ideal_thrive" }, // процвітання
+    { name: "ideal_development" }, // розвиток
+    { name: "ideal_knowledge" }, // знання
+    { name: "ideal_logics" }, // логіка
+    { name: "ideal_truth" }, // правда
+    { name: "ideal_creativity" }, // творчість
+    { name: "ideal_beauty" }, // краса
+    { name: "ideal_nature" }, // природа
+    { name: "ideal_harmony" }, // лагода
+    { name: "ideal_family" }, // сімʼя
+    { name: "ideal_friendship" }, // дружба
+    { name: "ideal_team" }, // команда
+    { name: "ideal_homeland" }, // батьківщина
+    { name: "ideal_altruism" }, // альтруїзм
+    { name: "ideal_mercy" }, // милосердя
+    { name: "ideal_destiry" }, // доля
+    { name: "ideal_faith" }, // віра
+    { name: "ideal_traditions" }, // традиції
+    { name: "ideal_duty" }, // обовʼязок
+    { name: "ideal_honor" }, // честь
+    { name: "ideal_revenge" }, // помста
+    { name: "ideal_strength" }, // сила
+    { name: "ideal_authority" }, // авторітет
+    { name: "ideal_power" }, // влада
+    { name: "ideal_glory" }, // слава
+    { name: "ideal_career" }, // карʼєра
+    { name: "ideal_melancholy" }, // користь
+  ],
 
-        { name: "ideal_justice",},// справедливість
-        { name: "ideal_equality",},// рівність
+  commitment: [
+    // ПРИХИЛЬНІСТЬ
 
-        { name: "ideal_beauty",},// краса
-        { name: "ideal_traditions",},// традиції
-        { name: "ideal_creativity",},// творчість
-        { name: "ideal_melancholy",},// користь
-        { name: "ideal_harmony",},// лагода
-        { name: "ideal_truth",},// правда
-        
-        { name: "ideal_respect",},// повага
-        { name: "ideal_society",},// суспільство
-        { name: "ideal_revenge",},// помста
-        { name: "ideal_career",},// карʼєра
-        { name: "ideal_authority",},// авторітет
-        { name: "ideal_independence",},// незалежність
-        { name: "ideal_liberty",},// свобода
-        { name: "ideal_power",},// влада
-        { name: "ideal_family",},// сімʼя
-        { name: "ideal_duty",},// обовʼязок
-        { name: "ideal_altruism",},// альтруїзм
-        { name: "ideal_freedom",},// воля
-        { name: "ideal_development",},// розвиток
-        { name: "ideal_team",},// команда
-        { name: "ideal_knowledge",},// знання
-        { name: "ideal_logics",},// логіка
-        { name: "ideal_strength",},// сила
-        { name: "ideal_destiry",},// доля
-        { name: "ideal_honor",},// честь
-        { name: "ideal_mercy",},// милосердя
-        { name: "ideal_faith",},// віра
-        { name: "ideal_homeland",},// батьківщина
-        { name: "ideal_thrive",},// процвітання
-        { name: "ideal_nature",},// природа
-        { name: "ideal_glory",},// слава
-        { name: "ideal_friendship",},// дружба
+    { name: "attachment_tool" }, // інструмент
+    { name: "attachment_vehicle" }, // корабель або кінь або брічка
+    { name: "attachment_corner" }, // свій куточок (майстерня)
+    { name: "attachment_childhood" }, // дитинство
+    { name: "attachment_school" }, // школа
+    { name: "attachment_offender" }, // кривдник
+    { name: "attachment_loss" }, // втрата
+    { name: "attachment_savior" }, // рятівник
+    { name: "attachment_leader" }, // лідер
+    { name: "attachment_mentor" }, // наставник
+    { name: "attachment_legacy" }, // спадок
+    { name: "attachment_child" }, // дитина
+    { name: "attachment_family" }, // сімʼя
+    { name: "attachment_home" }, // дім
+    { name: "attachment_bonds" }, // звʼязки
+    { name: "attachment_friends" }, // друзі
+    { name: "attachment_team" }, // команда
+    { name: "attachment_organization" }, // організація
+    { name: "attachment_homeland" }, // батьківщина
+    { name: "attachment_nature" }, // природа
+    { name: "attachment_love" }, // кохання
+    { name: "attachment_gift" }, // подарунок
+    { name: "attachment_fans" }, // прихильники
+    { name: "attachment_students" }, // учні
+    { name: "attachment_product" }, // плід праці
+    { name: "attachment_title" }, // титул
+    { name: "attachment_mistery" }, // загадка
+    { name: "attachment_idol" }, // кумир
+    { name: "attachment_artifact" }, // артефакт
+    { name: "attachment_structure" }, // сакральна споруда
+  ],
 
+  secret: [
+    // СЕКРЕТИ
+    { name: "secret_mistake" }, // жахлива помилка
+    { name: "secret_setup" }, // підстава
+    { name: "secret_crime" }, // злочин
+    { name: "secret_trait" }, // зрада
+    { name: "secret_owe" }, // борг
+    { name: "secret_shame" }, // ганьба
+    { name: "secret_exile" }, // вигнання
+    { name: "secret_follower" }, // переслідувач
+    { name: "secret_sick" }, // хвороба
+    { name: "secret_pact" }, // угода
+    { name: "secret_target" }, // мішень
+    { name: "secret_plans" }, // коварні плани
+    { name: "secret_prophesy" }, // пророцтво
+  ],
 
-        // ПРИХИЛЬНІСТЬ
-        { name: "attachment_",},// інструмент
-        { name: "attachment_",},// прихильники
-        { name: "attachment_",},// кумир
-        { name: "attachment_",},// команда
-        { name: "attachment_",},// друзі
-        { name: "attachment_",},// дім
-        { name: "attachment_",},// дитинство
-        { name: "attachment_",},// рятівник
-        { name: "attachment_",},// сімʼя
-        { name: "attachment_",},// звʼязки
-        { name: "attachment_",},// кохання
-        { name: "attachment_",},// лідер
-        { name: "attachment_",},// школа
-        { name: "attachment_",},// подарунок
-        { name: "attachment_",},// свій куточок (майстерня або приміщення, де крадій зберігає свій навар)
-        { name: "attachment_",},// організація
-        { name: "attachment_",},// батьківщина
-        { name: "attachment_",},// кривдник
-        { name: "attachment_",},// корабель або кінь або брічка
-        { name: "attachment_",},// учні
-        { name: "attachment_",},// артефакт
-        { name: "attachment_",},// сакральна споруда
-        { name: "attachment_",},// плід праці
-        { name: "attachment_",},// загадка
-        { name: "attachment_",},// втрата
-        { name: "attachment_",},// природа
-        { name: "attachment_",},// спадок
-        { name: "attachment_",},// дитина
-        { name: "attachment_",},// наставник
-        { name: "attachment_",},// титул
+  weakness: [
+    // СЛАБОСТИ
 
+    { name: "weakness_greed" }, // жадность
+    { name: "weakness_lust" }, // похоть
+    
+    { name: "weakness_passion" }, // азарт
+    { name: "weakness_dependance" }, // залежність
+    { name: "weakness_spender" }, // транжира
 
-        // СЕКРЕТИ
-        { name: "secret_",},// злочин
-        { name: "secret_",},// угода
-        { name: "secret_",},// переслідувач
-        { name: "secret_",},// пророцтво
-        { name: "secret_",},// ганьба
-        { name: "secret_",},// мішень
-        { name: "secret_",},// зрада
-        { name: "secret_",},// підстава
-        { name: "secret_",},// вигнання
-        { name: "secret_",},// коварні плани
-        { name: "secret_",},// жахлива помилка
-        { name: "secret_",},// борг
+    { name: "weakness_unreliability" }, // ненадежность
+    { name: "weakness_cowardice" }, // трусость
+    { name: "weakness_submissiveness" }, // покорність
+    
+    { name: "weakness_pride" }, // гординя
+    { name: "weakness_uncompromise" }, // бескомпромісний
+    { name: "weakness_audacity" }, // дерзость
+    { name: "weakness_wrath" }, // гнів
+    { name: "weakness_envy" }, // заздрість
 
-        // СЛАБОСТИ
-        { name: "weakness_",},// жадность
-        { name: "weakness_",},// гординя
-        { name: "weakness_",},// азарт
-        { name: "weakness_",},// лень
-        { name: "weakness_",},// похоть
-        { name: "weakness_",},// дерзость
-        { name: "weakness_",},// ненадежность
-        { name: "weakness_",},// трусость
-        { name: "weakness_",},// гнів
-        { name: "weakness_",},// заздрість
-        { name: "weakness_",},// покорність
-        { name: "weakness_",},// залежність
-        { name: "weakness_",},// транжира
-        { name: "weakness_",},// доверчивость
-        { name: "weakness_",},// беспечность
-        { name: "weakness_",},// безрассудство
-        { name: "weakness_",},// одержимость
-        { name: "weakness_",},// усложнительство
-        { name: "weakness_",},// бескомпромісний
-        { name: "weakness_",},// забивчівость
-    ],
+    { name: "weakness_belief" }, // доверчивость
+    { name: "weakness_laziness" }, // лень
+    { name: "weakness_poormemory" }, // забивчівость
+    { name: "weakness_carelessness" }, // беспечность
+    { name: "weakness_recklessness" }, // безрассудство
+    { name: "weakness_obsession" }, // одержимость
+    
+  ],
 };
