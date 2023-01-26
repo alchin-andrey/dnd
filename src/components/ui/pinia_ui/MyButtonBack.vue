2bfa91e2386193360c69c327b43fb0578b6c2782
Alchin Andrey <immortiale@gmail.com>
2023-01-26 03:41:09 +0300

    button back gradient

--- src/components/ui/pinia_ui/MyButtonBack.vue
+++ src/components/ui/pinia_ui/MyButtonBack.vue
@@ -1,45 +1,53 @@
 <template>
-	<div class="btm jbm-300">
-		<div>
-			<img
-				alt="arrow_left_small"
-				src="@/assets/img/icon/arrow_left_small.svg"
-			/>
+	<section class="btm">
+		<div class="gradient"></div>
+		<div class="btm_text jbm-300">
+			<div>
+				<img
+					alt="arrow_left_small"
+					src="@/assets/img/icon/arrow_left_small.svg"
+				/>
+			</div>
+			<div class="title">{{ t_Title }}</div>
 		</div>
-		<div class="title">{{t_Title}}</div>
-	</div>
+	</section>
 </template>
 
 <script>
 export default {
 	name: "MyButtonBack",
-  props: {
-    title: {
-      type: String,
-      default: null,
-    },
-  },
-  computed: {
-    t_Title() {
+	props: {
+		title: {
+			type: String,
+			default: null,
+		},
+	},
+	computed: {
+		t_Title() {
 			return this.t(this.title);
 		},
-  }
+	},
 };
 </script>
 
 <style scoped>
 .btm {
-	height: 18px;
-	margin: 0 0 32px 32px;
+	position: relative;
+}
+.btm_text {
+	height: 64px;
+	padding: 14px 0 0 32px;
 	display: flex;
 	cursor: pointer;
-	width: 52px;
+	/* width: 52px; */
 }
 
-.numb {
-	margin-top: 32px;
-	height: 18px;
-	text-align: left;
+.gradient {
+	height: 32px;
+	width: 100%;
+	position: absolute;
+	top: -32px;
+	background: linear-gradient(180deg, rgba(14, 21, 24, 0) 0%, #0e1518 100%);
 }
 
 .title {
