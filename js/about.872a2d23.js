7a854dd22abed89ca2939e9fcf105e81a8df44de
alchin-andrey <alchin-andrey@users.noreply.github.com>
2023-03-29 01:44:17 +0000

    deploy: 1346f6f6900867b7cef41f1142df0355700b6139

--- js/about.872a2d23.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkdnd"]=self["webpackChunkdnd"]||[]).push([[443],{697381:function(n,u,t){t.r(u),t.d(u,{default:function(){return f}});var a=t(373396);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var d=t(940089);const i={},o=(0,d.Z)(i,[["render",c]]);var f=o}}]);
-//# sourceMappingURL=about.872a2d23.js.map
