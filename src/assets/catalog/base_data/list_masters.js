6b2f70805a122245c00a9a94ae68c30fbdbe4d24
dimalagoda <dimalagoda@gmail.com>
2023-04-02 02:03:20 +0300

    lobby_dic_ua-only-please

---
+++ src/assets/catalog/base_data/list_masters.js
@@ -0,0 +1,10 @@
+export default {
+
+  andrii_pavlov: {
+    name: "andrii_pavlov",
+    details: "andrii_pavlov_details",
+    city: ["kyiv"],
+    format: ["offline","online"],
+    telegram: "andrii_pavlov_contact",
+  }
+};
