e916d2529c146573f59e60ac895311e8eefd1ff9
Alchin Andrey <immortiale@gmail.com>
2023-04-07 15:57:22 +0300

    Btm__Reset bottom

--- src/components/settings/0_settings__header/Description.vue
+++ src/components/settings/0_settings__header/Description.vue
@@ -3,8 +3,8 @@
 		<section class="flex-col gap-26">
       <Welcome/>
       <div class="flex-col gap-4">
-        <Btm__Reset />
         <Btm__WhatDND />
+        <Btm__Reset />
       </div>
 		</section>
 		<Donate />
@@ -13,8 +13,8 @@
 
 <script>
 import Welcome from "@/components/Welcome.vue";
-import Btm__Reset from "@/components/Btm__Reset.vue";
 import Btm__WhatDND from "@/components/Btm__WhatDND.vue";
+import Btm__Reset from "@/components/Btm__Reset.vue";
 import Donate from "@/components/Donate.vue";
 export default {
 	components: { 
