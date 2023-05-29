71bfbc6e14e297d45782aba679dff7fbdeb50f00
dimalagoda <dimalagoda@gmail.com>
2023-05-29 13:52:07 +0300

    photo_links

---
+++ src/assets/catalog/base_data/list_links.js
@@ -0,0 +1,52 @@
+export default {
+
+none: {
+
+  gnome: {
+    male:"https://www.heroforge.com/load_config%3D503227559/",
+    female:"https://www.heroforge.com/load_config%3D503227430/",
+  },
+
+  halfling: {
+    male:"https://www.heroforge.com/load_config%3D503210924/",
+    female:"https://www.heroforge.com/load_config%3D503211388/",
+  },
+
+  dwarf: {
+    male:"https://www.heroforge.com/load_config%3D503227908/",
+    female:"https://www.heroforge.com/load_config%3D503228037/",
+  },
+
+  human: {
+    male:"https://www.heroforge.com/load_config%3D503227522/",
+    female:"https://www.heroforge.com/load_config%3D503227396/",
+  },
+
+  halfelf: {
+    male:"https://www.heroforge.com/load_config%3D503597041/",
+    female:"https://www.heroforge.com/load_config%3D503597082/",
+  },
+
+  elf: {
+    male:"https://www.heroforge.com/load_config%3D503597031/",
+    female:"https://www.heroforge.com/load_config%3D503597004/",
+  },
+
+  tiefling: {
+    male:"https://www.heroforge.com/load_config%3D506872104/",
+    female:"https://www.heroforge.com/load_config%3D503227181/",
+  },
+
+  halforc: {
+    male:"https://www.heroforge.com/load_config%3D503597221/",
+    female:"https://www.heroforge.com/load_config%3D503597128/",
+  },
+
+  dragonborn: {
+    male:"https://www.heroforge.com/load_config%3D503517832",
+    female:"https://www.heroforge.com/load_config%3D503517264",
+  },
+
+}
+
+};
