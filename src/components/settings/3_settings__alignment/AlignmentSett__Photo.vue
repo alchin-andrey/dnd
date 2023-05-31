a898bd65449aebb480d760b7d529eba63b11ed32
alchin-andrey <immortiale@gmail.com>
2023-06-01 00:37:46 +0300

    enter_url

--- src/components/settings/3_settings__alignment/AlignmentSett__Photo.vue
+++ src/components/settings/3_settings__alignment/AlignmentSett__Photo.vue
@@ -2,13 +2,10 @@
 	<AppCardWrapp :active_card="!site_settings.photo_user" @click="getPhotoStatus(false)">
 		<div class="int-700">{{ T('standard') }}</div>
 	</AppCardWrapp>
-	<AppCardWrapp 
-	gap="26" 
-	:active_card="site_settings.photo_user" 
-	:passive="!active_Custom_Photo || site_settings.photo_user"
-	@click="getPhotoStatus(active_Custom_Photo)"
-	>
+	<AppCardWrapp gap="26" :active_card="site_settings.photo_user"
+		:passive="!active_Custom_Photo || site_settings.photo_user" @click="getPhotoStatus(active_Custom_Photo)">
 		<main class="flex-col gap-16">
+			
 			<section :class="['input-box', stule_Hov]" :style="stule_Img_Obj">
 				<label for="">
 					<input type="file" id="myFile" size="50" accept="image/*" @change="onChange($event)">
@@ -16,34 +13,24 @@
 				</label>
 				<div v-if="MY.custom_photo" class="plag-photo-load"></div>
 				<template v-if="size_Cover && MY.custom_photo">
-					<AppRangPhoto
-					:class="[style_Rang_Photo]"
-					v-if="pos_Rang_Photo"
-					v-model.number="site_settings.photo_sett.position"
-					:orientation="pos_Rang_Photo"
-					:pad="padding_Rang_Photo"
-					@click.stop
-					/>
-					
+					<AppRangPhoto :class="[style_Rang_Photo]" v-if="pos_Rang_Photo"
+						v-model.number="site_settings.photo_sett.position" :orientation="pos_Rang_Photo" :pad="padding_Rang_Photo"
+						@click.stop />
 				</template>
 			</section>
+			
 			<section class="grit-btm" v-if="MY.custom_photo">
-				<AppBtmIcon
-					icon="photo_fill"
-					@click="getPosition(true)"
-					:active_btm="site_settings.photo_sett.size_cover"
-					@click.stop
-				/>
-				<AppBtmIcon
-					icon="photo_fit"
-					@click="getPosition(false)"
-					:active_btm="!site_settings.photo_sett.size_cover"
-					@click.stop
-				/>
-				<AppBtmIcon icon="delete" @click="delPhoto()" @click.stop/>
+				<AppBtmIcon icon="photo_fill" @click="getPosition(true)" :active_btm="site_settings.photo_sett.size_cover"
+					@click.stop />
+				<AppBtmIcon icon="photo_fit" @click="getPosition(false)" :active_btm="!site_settings.photo_sett.size_cover"
+					@click.stop />
+				<AppBtmIcon icon="delete" @click="delPhoto()" @click.stop />
 			</section>
+			<label class="photo-url" for="url">
+				<input v-if="!MY.custom_photo" type="url" name="url" id="url" class="int-700" :placeholder="T('enter_url')" pattern="https://.*" size="30"
+				required @change="onChangeUrl($event)">
+			</label>
 		</main>
-		<!-- <input class="int-400" type="url" name="url" id="url" placeholder="Введіть URL" pattern="https://.*" size="30" required @change="onChangeUrl($event)"> -->
 
 		<section>
 			<div class="int-700 pos-rel">{{ T('your_image') }}</div>
@@ -94,7 +81,7 @@ export default {
 					'background-position': `${pos_num} ${pos_num}`,
 				}
 			}
-				return {'background-image': this.upload};
+			return { 'background-image': this.upload };
 		},
 
 		stule_Hov() {
@@ -120,18 +107,18 @@ export default {
 		},
 
 		pos_Rang_Photo() {
-			if(this.ratio_Rang_Photo < 0) return 'horizontal';
-			if(this.ratio_Rang_Photo > 0) return 'vertical';
+			if (this.ratio_Rang_Photo < 0) return 'horizontal';
+			if (this.ratio_Rang_Photo > 0) return 'vertical';
 		},
 
 		padding_Rang_Photo() {
-			if(this.ratio_Rang_Photo < 0) return 10;
-			if(this.ratio_Rang_Photo > 0) return 8;
+			if (this.ratio_Rang_Photo < 0) return 10;
+			if (this.ratio_Rang_Photo > 0) return 8;
 		},
 
 		style_Rang_Photo() {
-			if(this.ratio_Rang_Photo < 0) return 'rang-rl';
-			if(this.ratio_Rang_Photo > 0) return 'rang-tb';
+			if (this.ratio_Rang_Photo < 0) return 'rang-rl';
+			if (this.ratio_Rang_Photo > 0) return 'rang-tb';
 		},
 	},
 
@@ -148,10 +135,10 @@ export default {
 				reader.addEventListener("load", (el) => {
 					if (el.target.result) {
 						const image = new Image();
-            image.src = el.target.result;
-            image.addEventListener("load", (e) => {
+						image.src = el.target.result;
+						image.addEventListener("load", (e) => {
 							this.site_settings.photo_sett.ratio = e.target.width / e.target.height;
-            });
+						});
 						this.MY.custom_photo = el.target.result;
 						this.site_settings.photo_user = true;
 					}
@@ -159,19 +146,25 @@ export default {
 			}
 		},
 
-		// onChangeUrl(event) {
-		// 	this.MY.custom_photo = event.target.value
-		// },
+		onChangeUrl(event) {
+			const image = new Image();
+			image.src = event.target.value;
+			image.addEventListener("load", (e) => {
+				this.site_settings.photo_sett.ratio = e.target.width / e.target.height;
+				this.MY.custom_photo = event.target.value;
+				this.site_settings.photo_user = true;
+			});
+		},
 
 		getPosition(bool) {
 			this.site_settings.photo_sett.size_cover = bool;
 		},
 
 		showEdit() {
-			if(this.size_Cover) {
+			if (this.size_Cover) {
 				this.edit_visible = !this.edit_visible;
 			}
-    },
+		},
 
 		delPhoto() {
 			this.site_settings.photo_sett.size_cover = true;
@@ -190,7 +183,7 @@ export default {
 .grit-btm {
 	display: grid;
 	grid-template-columns: repeat(3, 1fr);
-	grid-template-rows: 47px;
+	grid-template-rows: 48px;
 	gap: 8px;
 }
 
@@ -256,9 +249,9 @@ export default {
 	transition-duration: .5s;
 }
 
-.hov label:hover{
+.hov label:hover {
 	background-color: rgba(255, 255, 255, 0.06);
-  }
+}
 
 /* .hov-img label:hover{
   background-image: url("data:image/svg+xml,%3Csvg width='36' height='54' viewBox='0 0 36 54' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 26.3251L2.30369 28.6283L16.1903 14.4785V53.9664H19.4809V14.4127L33.6307 28.6283L36 26.2591L17.8356 8.09465L0 26.3251Z' fill='white'/%3E%3Cpath d='M1.11882 0H34.8151V3.29065H1.11882V0Z' fill='white'/%3E%3C/svg%3E");
@@ -267,11 +260,18 @@ export default {
 	background-position: 50%, 50%;
   } */
 
-/* input[type=url] {
+input[type=url] {
 	width: 100%;
+	height: 48px;
 	background-color: rgba(255, 255, 255, 0.06);
-	border-radius: 4px;
-	border: 1px solid rgba(255, 255, 255, 0.1);
-	padding: 4px;
-} */
+	border-radius: 12px;
+	/* border: 1px solid rgba(255, 255, 255, 0.1); */
+	padding: 8px;
+	color: #ffffff;
+}
+
+.photo-url:hover {
+	background-color: rgba(255, 255, 255, 0.06);
+	transition-duration: .5s;
+}
 </style>
