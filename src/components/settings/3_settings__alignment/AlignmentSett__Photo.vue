881ca0d17c1cbe9ff5bdec885843ffbb11475ebf
alchin-andrey <immortiale@gmail.com>
2023-06-01 04:56:15 +0300

    pass url fix

--- src/components/settings/3_settings__alignment/AlignmentSett__Photo.vue
+++ src/components/settings/3_settings__alignment/AlignmentSett__Photo.vue
@@ -175,7 +175,6 @@ export default {
 			// const link = event.clipboardData.getData('Text')
 			setTimeout(() => {
 				const image = new Image();
-				console.log('event.target.value:', event.target.value)
 				image.src = event.target.value;
 				image.addEventListener("load", (e) => {
 					this.site_settings.photo_sett.ratio = e.target.width / e.target.height;
