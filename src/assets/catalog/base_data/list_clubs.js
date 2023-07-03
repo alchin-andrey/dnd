73dda6ee6a65e18c0bf109ae34a94baf25c6efa7
dimalagoda <dimalagoda@gmail.com>
2023-07-03 13:09:36 +0300

    clubs reorder

--- src/assets/catalog/base_data/list_clubs.js
+++ src/assets/catalog/base_data/list_clubs.js
@@ -1,13 +1,5 @@
 export default {
 
-  did: {
-    id: 1,
-    name: "club_did_name",
-    details: "club_did_details",
-    link_name: "dedlihozor.club",
-    link: "https://dedlihozor.club"
-  },
-
   huhy: {
     id: 5,
     name: "club_huhy_name",
@@ -16,20 +8,28 @@ export default {
     link: "https://t.me/huhyclub",
   },
 
-  mr: {
-    id: 6,
-    name: "club_mr_name",
-    details: "club_mr_details",
-    link_name: "@Lviv_MR_DnD",
-    link: "https://t.me/Lviv_MR_DnD/9310",
-  },
-
   eneri: {
     id: 2,
     name: "club_eneri_name",
     details: "club_eneri_details",
     link_name: "eneri.com.ua",
     link: "https://eneri.com.ua",
+  },
+
+  did: {
+    id: 1,
+    name: "club_did_name",
+    details: "club_did_details",
+    link_name: "dedlihozor.club",
+    link: "https://dedlihozor.club"
+  },
+
+  mr: {
+    id: 6,
+    name: "club_mr_name",
+    details: "club_mr_details",
+    link_name: "@Lviv_MR_DnD",
+    link: "https://t.me/Lviv_MR_DnD/9310",
   },
 
   rumble_dice: {
