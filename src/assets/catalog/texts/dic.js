8ba100897d5404a2df695956d5a1d54adc836a60
dimalagoda <dimalagoda@gmail.com>
2023-10-05 14:44:12 +0300

    keep ua only, fiend update deploy

--- src/assets/catalog/texts/dic.js
+++ src/assets/catalog/texts/dic.js
@@ -1,15 +1,11 @@
 import ua_dic from './ua/_dic.js';
 import ua_inrium_dic from './ua_inrium/_dic.js';
-import ru_dic from './ru/_dic.js';
-import en_dic from './en/_dic.js';
 
 
 export default {
 
     ua: ua_dic,
     ua_inrium: ua_inrium_dic,
-    ru: ru_dic,
-    en: en_dic,
 
     lang: [
         {
