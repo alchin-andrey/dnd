1224eff3475ef686aeb147d2d8ddd183dad33700
dimalagoda <dimalagoda@gmail.com>
2026-01-31 16:45:59 +0200

    promtupd

--- src/assets/catalog/base_data/list_prompts.js
+++ src/assets/catalog/base_data/list_prompts.js
@@ -4,8 +4,6 @@ export default {
 
     prompt: "Write a concise Dungeons & Dragons character biography (max 2000 characters) using the attributes provided. It should be whole, consistent, and have non-standard but reasonable cause and effect relationship for each phase, person, event, place, for difficulties, challenges, experiences, traumas, successes, failures, goals, purpose etc. Third-person voice, past to present. Use more specific names, events and toponyms. Don’t use gender and alignment names straight, describe indirectly. Weave motivations, relationships, secrets and a signature quirk. Keep it generic but vivid. End with a one-sentence adventure hook, integrated in the story. Language of output — Ukrainian.", //add dic name?
 
-    
-
   },
 
 };
