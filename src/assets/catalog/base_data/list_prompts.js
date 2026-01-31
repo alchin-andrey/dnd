4b841d014257a65628fcf0a9eb423dcc8d1eb389
dimalagoda <dimalagoda@gmail.com>
2026-02-01 01:28:45 +0200

    prompt_short

--- src/assets/catalog/base_data/list_prompts.js
+++ src/assets/catalog/base_data/list_prompts.js
@@ -2,7 +2,7 @@ export default {
 
   bio: {
 
-    prompt: "Write a concise Dungeons & Dragons character biography (max 2000 characters) using the attributes provided. It should be whole, consistent, and have non-standard but reasonable cause and effect relationship for each phase, person, event, place, for difficulties, challenges, experiences, traumas, successes, failures, goals, purpose etc. Third-person voice, past to present. Use more specific names, events and toponyms. Don’t use gender and alignment names straight, describe indirectly. Weave motivations, relationships, secrets and a signature quirk. Keep it generic but vivid. End with a one-sentence adventure hook, integrated in the story. Language of output — Ukrainian.", //add dic name?
+    prompt: "Write D&D char bio using the attributes provided. It is whole, consistent, and has non-standard but reasonable cause and effect relationship for each phase, person, event, place, for difficulties, challenges, experiences, traumas, successes, failures, goals, purpose etc. 3rd-person voice, past to present. More specific names, events and toponyms. Description of gender and alignment is indirect. Weave motivations, relationships, secrets and a signature quirk. Keep it generic but vivid. End with a one-sentence adventure hook, integrated in the story. Lang - UA.", //add dic name?
 
   },
 
