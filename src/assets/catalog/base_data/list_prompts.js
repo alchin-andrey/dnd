3c954086a3913b667206560f16ae2dc0888d7e7d
dimalagoda <dimalagoda@gmail.com>
2026-02-04 23:38:27 +0200

    prompt fix i hope

--- src/assets/catalog/base_data/list_prompts.js
+++ src/assets/catalog/base_data/list_prompts.js
@@ -2,7 +2,7 @@ export default {
 
   bio: {
 
-    prompt: "Write detailed D&D char bio using the attributes provided. It is whole, consistent, and has non-standard but reasonable cause and effect relationship for each phase, person, event, place, for difficulties, challenges, experiences, traumas, successes, failures, goals, purpose etc. 3rd-person voice, past to present. Less poetic, more straight forward. More specific names, events and toponyms. Description of gender and alignment is indirect. Weave motivations, relationships, secrets and a signature quirk. Keep it generic but vivid. End with a one-sentence adventure hook, integrated in the story. Lang - UA. 7 long paragraphs, about 800 letters each.", //add dic name?
+    prompt: "Write detailed D&D char bio using the attributes provided. It is whole, consistent, and has non-standard but reasonable cause and effect relationship for each phase, person, event, place, for difficulties, challenges, experiences, traumas, successes, failures, goals, purpose etc. 3rd-person voice, past to present. Less poetic, more straight forward. More specific names, events and toponyms. Description of gender, level and alignment is indirect. Weave motivations, relationships, secrets and a signature quirk. Keep it generic but vivid. End with a one-sentence adventure hook, integrated in the story. Lang - UA. 7 long paragraphs, about 700 letters each.", //add dic name?
 
   },
 
