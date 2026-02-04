78676ae8ec802e6f6046b434df5d3615b89510e2
alchin-andrey <immortiale@gmail.com>
2026-02-04 16:27:35 +0300

    AppBtmLink__DonateBaner hover

--- src/components/ui/AppBtmLink__DonateBaner.vue
+++ src/components/ui/AppBtmLink__DonateBaner.vue
@@ -6,7 +6,15 @@
 	href="https://cutt.ly/lwkDE0Yn">
 	<div class="flex-row-sb">
 		<div class="int-400 color-purple" >{{ T('banner_title') }}</div>
-		<div class="int-400" >{{ T('banner_cta') }}</div>
+		<div class="flex-row">
+			<div class="int-400" >{{ T('banner_cta') }}</div>
+				<AppSvg 
+					class="svg-15 svg-main-f"
+					name="donate"
+					box_size="15"
+					box_y="1"
+				/>
+		</div>
 	</div>
 	</a>
 </template>
--- src/components/ui/AppSvg.vue
+++ src/components/ui/AppSvg.vue
@@ -1,6 +1,6 @@
 <template>
 	<svg
-		viewBox="0 0 18 18"
+		:viewBox="view_Box"
 		xmlns="http://www.w3.org/2000/svg"
 		v-html="svg_Path"
 	></svg>
@@ -24,11 +24,32 @@ export default {
 			type: String,
 			default: 'close',
 		},
+
+		box_x: {
+			type: String,
+			default: '0',
+		},
+
+		box_y: {
+			type: String,
+			default: '0',
+		},
+
+		box_size: {
+			type: String,
+			default: '18',
+		},
+
 	},
 	computed: {
     svg_Path() {
 			return this[this.catalog][this.name]
+		},
+
+		view_Box() {
+			return `${this.box_x} ${this.box_y} ${this.box_size} ${this.box_size}`
 		}
+		
   },
 };
 </script>
